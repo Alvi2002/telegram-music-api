774a0651c8ec97c779d8004d2fc9f14fe35c03bb
Alvi2002 <hasibsheikh400@gmail.com>
2026-06-25 07:37:47 +0600

    Initialize Express server with YouTube route

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import youtubeRoute from "./routes/youtube.js";
+
+
+dotenv.config();
+
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json());
+
+
+app.use(
+"/search",
+youtubeRoute
+);
+
+
+
+app.get("/",(req,res)=>{
+
+res.send(
+"Telegram Music API Running"
+);
+
+});
+
+
+
+const PORT =
+process.env.PORT || 5000;
+
+
+
+app.listen(PORT,()=>{
+
+console.log(
+`Server running on ${PORT}`
+);
+
+});
